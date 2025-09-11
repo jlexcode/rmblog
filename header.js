0cc9aeff6cad50590b0c512d9f683d7d6b68f2b0
LexMB <lexmb@LexMBs-MacBook-Pro.local>
2025-09-11 19:49:40 -0400

    Update post content

--- header.js
+++ header.js
@@ -3,8 +3,8 @@ function createHeader() {
     return `
         <nav class="mb-8 border-b border-black pb-4">
             <div class="flex space-x-6">
-                <a href="index.html" class="text-black font-['Space_Mono'] hover:underline">Home</a>
-                <a href="about.html" class="text-black font-['Space_Mono'] hover:underline">About</a>
+                <a href="/" class="text-black font-['Space_Mono'] hover:underline">Home</a>
+                <a href="/about.html" class="text-black font-['Space_Mono'] hover:underline">About</a>
             </div>
         </nav>
     `;
